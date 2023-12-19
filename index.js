d5eb53be54c1aada72b374f075426fd63559e944
Justiniano22 <138537824+Justiniano22@users.noreply.github.com>
2023-12-19 18:34:33 -0300

    Update Api Andando

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-const storage = getStorage();
-const imageRef = ref(storage, 'gs://platos-974f6.appspot.com/Redes Sociales.png');
-
-getDownloadURL(imageRef)
-  .then((url) => {
-    // Aquí puedes usar la URL para mostrar la imagen en tu sitio web
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.appendChild(img);
-  })
-  .catch((error) => {
-    console.error('Error al obtener la URL de la imagen', error);
-  });*/
-
-
-
 window.onscroll = function(){
     if(document.documentElement.scrollTop > 100){
         document.querySelector('.go-top-container').classList.add('show');
@@ -32,24 +15,8 @@ document.querySelector('.go-top-container')
 });
 
 
-const btnAnterior = document.getElementById("btnAnterior");
-const btnSiguiente = document.getElementById("btnSiguiente");
 const contenedor = document.getElementById("contenedor");
 let pagina = 1;
-
-btnAnterior.addEventListener("click", ()=>{
-    if(pagina > 1){
-        pagina -= 1;
-        cargarPlatos();
-    }
-})
-btnSiguiente.addEventListener("click", ()=>{
-    if(pagina < 1000){
-        pagina += 1;
-        cargarPlatos();
-    }
-})
-
 
 const cargarPlatos = async()=>{
 
@@ -70,8 +37,7 @@ const cargarPlatos = async()=>{
                     <div class="card-body">
                         <img src="${platos.enlaceFoto}" class="card-img-top" alt="...">
                         <h5 class="card-title titulos">${platos.nombrePlato}</h5>
-                        <p class="card-text descripcion">Descripcion: ${platos.enlaceFoto}</p>
-                        <p class="card-text descripcion">Descripcion: ${platos.descripcion}</p>
+                        <ph1 class="card-text descripcion">Descripcion: ${platos.descripcion}</p>
                         <p class="card-text descripcion">Ingredientes: ${platos.ingredientes}</p>
                         <p class="card-text descripcion">Elaboración: ${platos.elaboracion}</p>
                         <p class="card-text descripcion">Calorias: ${platos.calorias}</p>
